a2c08e3c734b68bc8b40fec87b54543ac20d8434
Jonathan Perry <jpper@umich.edu>
2024-03-27 17:16:53 -0500

    Implement section list with sticky header

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,8 +1,15 @@
-import { View, Text, Image } from "react-native";
+import { ScrollView, Text, Image, TouchableOpacity, View } from "react-native";
 import { Currency } from "@/interfaces/crypto";
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "expo-router";
+import { useHeaderHeight } from "@react-navigation/elements";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
+  const headerHeight = useHeaderHeight();
+
   const currencies = useQuery({
     queryKey: ["listings"],
     queryFn: () => fetch("/api/listings").then((res) => res.json()),
@@ -19,17 +26,57 @@ const Page = () => {
   });
 
   return (
-    <View>
-      {currencies.data?.map((currency: Currency) => (
-        <View style={{ flexDirection: "row" }} key={currency.id}>
-          <Image
-            source={{ uri: data?.[currency.id].logo }}
-            style={{ width: 32, height: 32 }}
-          />
-          <Text key={currency.id}>{currency.name}</Text>
-        </View>
-      ))}
-    </View>
+    <ScrollView
+      style={{ backgroundColor: Colors.background }}
+      contentContainerStyle={{ paddingTop: headerHeight }}
+    >
+      <Text style={defaultStyles.sectionHeader}>Latest Crypto</Text>
+
+      <View style={defaultStyles.block}>
+        {currencies.data?.map((currency: Currency) => (
+          <Link href={`/crypto/${currency.id}`} key={currency.id} asChild>
+            <TouchableOpacity
+              style={{ flexDirection: "row", gap: 14, alignItems: "center" }}
+            >
+              <Image
+                source={{ uri: data?.[currency.id].logo }}
+                style={{ width: 40, height: 40 }}
+              />
+              <View style={{ flex: 1, gap: 10 }}>
+                <Text style={{ fontWeight: "600", color: Colors.dark }}>
+                  {currency.name}
+                </Text>
+                <Text style={{ color: Colors.gray }}>{currency.symbol}</Text>
+              </View>
+              <View style={{ gap: 6, alignItems: "flex-end" }}>
+                <Text>{currency.quote.EUR.price.toFixed(2)} $</Text>
+                <View style={{ flexDirection: "row", gap: 4 }}>
+                  <Ionicons
+                    name={
+                      currency.quote.EUR.percent_change_1h > 0
+                        ? "caret-up"
+                        : "caret-down"
+                    }
+                    size={16}
+                    color={"green"}
+                  />
+                  <Text
+                    style={{
+                      color:
+                        currency.quote.EUR.percent_change_1h > 0
+                          ? "green"
+                          : "red",
+                    }}
+                  >
+                    {currency.quote.EUR.percent_change_1h.toFixed(2)}%
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </Link>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
