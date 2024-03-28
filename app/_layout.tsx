b7c3e3f3a397dde7bf895e2deb1f72b36f523378
Jonathan Perry <jpper@umich.edu>
2024-03-28 12:01:20 -0500

    Implement account page with image upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -67,7 +67,7 @@ const InitialLayout = () => {
     const inAuthGroup = segments[0] === "(authenticated)";
 
     if (isSignedIn && !inAuthGroup) {
-      router.replace("/(authenticated)/(modals)/lock");
+      router.replace("/(authenticated)/(tabs)/home");
     } else if (!isSignedIn) {
       router.replace("/");
     }
